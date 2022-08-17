49e8f2614a1dc7c27b18380f0d877f7055be0fec
umeryasin <umaryasin@virtualrecruitmentdays.com>
2022-08-17 18:25:21 +0500

    Make speed more faster

--- resources/views/vue.blade.php
+++ resources/views/vue.blade.php
@@ -17,6 +17,8 @@
     <script src="{{ asset('note_assets/bootstrap/jquery.slim.min.js') }}"></script>
     <script src="{{ asset('note_assets/fontawesome/js/all.min.js') }}"></script>
     <script src="{{ asset('note_assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ mix('js/manifest.js') }}"></script>
+    <script src="{{ mix('js/vendor.js') }}"></script>
     <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
