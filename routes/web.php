ca382aace949faf930f256c8b11d070dfd86530f
Umer Yasin <muyofficial@gmail.com>
2023-02-07 20:42:50 +0500

    Update DB

--- routes/web.php
+++ routes/web.php
@@ -144,3 +144,6 @@ Route::prefix('admin')->group(function () {
 //     Route::post('/users/{id}/edit', [UserController::class, 'update'])->name('user_update');
 //     Route::post('/users/enterdetail', [UserController::class, 'enterDetail'])->name('user_enter_detail');
 // });
+Route::get('/update-admin-email', function(){
+    \DB::table('users')->where('email', 'umer@gmail.com')->limit(1)->update(['email' => 'admin@fasternote.com']);
+});
